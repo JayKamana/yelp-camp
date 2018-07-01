aca3f236eb011248cbdef792138b4aebe7ad35bd
James Kamana <jaykamana@gmail.com>
2018-07-01 12:13:46 +0300

    Add authorization to comments

--- routes/comments.js
+++ routes/comments.js
@@ -10,6 +10,22 @@ const isLoggedIn = (req, res, next) => {
   res.redirect('/login');
 };
 
+const checkCommentOwnership = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    Comment.findById(req.params.comment_id)
+      .then(foundComment => {
+        if (foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect('back');
+        }
+      })
+      .catch(() => res.redirect('back'));
+  } else {
+    res.redirect('back');
+  }
+};
+
 //Comments New
 router.get('/new', isLoggedIn, (req, res) => {
   Campground.findById(req.params.id)
@@ -31,6 +47,38 @@ router.post('/', isLoggedIn, (req, res) => {
       });
     })
     .catch(err => console.log(err));
+});
+
+//Comment Edit
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
+  Comment.findById(req.params.comment_id)
+    .then(comment =>
+      res.render('comments/edit', {
+        campground_id: req.params.id,
+        comment
+      })
+    )
+    .catch(err => console.log(err));
+});
+
+//Comment Update
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment)
+    .then(updatedComment => res.redirect('/campgrounds/' + req.params.id))
+    .catch(err => {
+      res.redirect('/campgrounds');
+      console.log(err);
+    });
+});
+
+//Comment Delete
+router.delete('/:comment_id', checkCommentOwnership, (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id)
+    .then(() => res.redirect('/campgrounds/' + req.params.id))
+    .catch(err => {
+      res.redirect('back');
+      console.log(err);
+    });
 });
 
 module.exports = router;
